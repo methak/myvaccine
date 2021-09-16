24abe116cb0226b9ae2bcd0d6e0c3577181690f2
metha <matt.ksmp@gmail.com>
2021-09-16 16:54:16 -0700

    Created Provider Model

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Provider(models.Model):
+    name = models.CharField(max_length=100)
+    address = models.CharField(max_length=100)
+    phone = models.CharField(max_length=15)
+    appointment = models.BooleanField()
+    walkin = models.BooleanField()
+    hours = models.CharField(max_length=100)
